c5e5d21fd618fbab9f38e7f344bff4e265f52dfe
DmitriyShum <dmitriyshumkin@me.com>
2018-10-22 00:10:36 -0400

    account.py
    make a quick profile

---
+++ account.py
@@ -0,0 +1,7 @@
+# Register or login template!
+# Written in Python...
+##By: Dmitriy
+usrname = input("Please enter a username:")
+print(usrname, " Registered.")
+pssword = input("Please make a password:")
+print("Password registered.")
